1ce59f4b633bb69227a80a8fd39472d2ea6466a9
Rishabh-1999 <rishabhanand33@gmail.com>
2019-09-28 13:18:32 +0530

    Login Page Updated

--- app.js
+++ app.js
@@ -1,46 +1,140 @@
 var express = require('express');
 var path = require('path');
-var app = express()
 var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var engine = require('ejs-mate');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 var session = require('express-session'); 
 var mongoose = require('mongoose');
-var passport = require('passport');
 var mongoStore = require('connect-mongo')(session);
+var User   = require('./models/users');
+var middleware = require('./middlewares/middleware');
+var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect("mongodb://localhost:27017/socialmedia");
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection open");
+});
+
+// require('./secure/passport');
+
+app.set('port', process.env.PORT || 3000 );
+// view engine setup
+app.engine('ejs',engine);
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
-
-//Bodyparser
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json()); 
-
-var mongoDB = 'mongodb://localhost/myDB';
-
-var db=mongoose.connection.on('error', (err) => {
-    console.log('DB connection Error');
-});
-
-mongoose.connection.on('connected', (err) => {
-    console.log('DB connected');
-});
-
-mongoose.connect(mongoDB,{ useNewUrlParser: true });
 
 app.use(session({
-    secret: "abcUCAChitkara",
-    resave: true,
-    saveUninitialized: true,
-    store : new mongoStore({mongooseConnection:db})
-  }));
-
+  secret: '6sd65d6f45e95',
+  resave: true,
+  saveUninitialized: false,
+  store : new mongoStore({mongooseConnection:db})
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-console.log("Running on port 3000");
-app.listen(3000)
+
+/*
+* Express Validator - Error Formatter Middleware - START
+*(the formParam value is going to get morphed into form body format useful for printing.)
+*/
+
+// app.use(validator({
+//   errorFormatter: function(param, msg, value) {
+//       var namespace = param.split('.')
+//       , root    = namespace.shift()
+//       , formParam = root;
+
+//     while(namespace.length) {
+//       formParam += '[' + namespace.shift() + ']';
+//     }
+//     return {
+//       param : formParam,
+//       msg   : msg,
+//       value : value
+//     };
+//   },
+//   customValidators: {
+ 
+//     isExist_email : function(email){
+        
+//       return new Promise(function(resolve,reject){
+          
+//           User.findOne({'email':email},(err,user) => {
+
+//               if(err) throw err;
+
+//               if(user){
+//                 return reject();
+//               };
+
+//               return resolve();
+//           });            
+        
+//       });
+
+//     }  
+
+//  }
+// }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  if(req.user) res.locals.session_id = req.user._id;
+  next();
+});
+
+// route Path
+var index = require('./routes/index');
+// var users = require('./routes/users');
+// var search = require('./routes/search');
+// var token  = require('./routes/tokens');
+// var post  = require('./routes/post');
+
+
+app.use('/', index);
+// app.use('/', users);
+// app.use('/', search);
+// app.use('/', token);
+// app.use('/', post);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error',{title:'Page404',header:false});
+});
+
+app.listen(app.get('port'),function(){
+	console.log("Server is running on port "+app.get('port'));
+})
 
 module.exports = app;
---
+++ secure/methods.js
@@ -0,0 +1,50 @@
+
+var nodemailer = require('nodemailer');
+var crypto = require('crypto'); 
+require('dotenv').config()
+
+// var get    = require('../secure/smtp-cred');
+
+function token(email){
+	var secret = email.split("@");
+                secret = secret[0] + Date.now();
+    var hash = crypto.createHmac('sha256', secret)
+                           .update(secret[1])
+                           .digest('hex');
+    return hash = hash.substr(20,40);
+}
+
+function sendMail(mailOptions){
+    return new Promise(function(resolve,reject){
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "rishabhanand33@gmail.com",
+                pass: process.env.password
+            },
+            tls: {
+                rejectUnauthorized : false
+            }
+        });
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(info);       
+        });
+    });
+}
+
+function saveUser(profile,newUser,hash){
+                newUser.fullname = profile.displayName;
+                newUser.email = profile.emails[0].value;
+                newUser.image = profile.photos[0].value;
+                newUser.status = "1";
+                return newUser;
+}
+
+
+module.exports.token = token;
+module.exports.sendMail = sendMail;
+module.exports.saveUser = saveUser;
